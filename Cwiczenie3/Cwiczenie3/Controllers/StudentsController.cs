c0a5f1c44f9de740102c8110f61226af89a80a75
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-12-08 11:52:40 +0100

    URL segment

--- Cwiczenie3/Cwiczenie3/Controllers/StudentsController.cs
+++ Cwiczenie3/Cwiczenie3/Controllers/StudentsController.cs
@@ -15,5 +15,18 @@ namespace Cwiczenie3.Controllers
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Kowalski");
+            }else if(id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
     }
 }
